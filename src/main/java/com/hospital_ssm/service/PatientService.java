fa8049429a1a6b6f28733979c35098bca2531c28
yjbjb <1466749056@qq.com>
2022-05-06 13:35:45 +0000

    update src/main/java/com/hospital_ssm/service/PatientService.java. service

--- src/main/java/com/hospital_ssm/service/PatientService.java
+++ src/main/java/com/hospital_ssm/service/PatientService.java
@@ -2,6 +2,7 @@ package com.hospital_ssm.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hospital_ssm.pojo.Patient;
+import java.util.List;
 
 public interface PatientService {
 
@@ -12,4 +13,20 @@ public interface PatientService {
     Integer updatePatient(Patient patient);
 
     Integer removePatient(Patient patient);
+
+    //依据id的五个基本方法
+	public int addPatient(Patient patient) ;
+	public int deletePatient(Integer id);
+	public int updatePatient(Patient patient);
+	public Patient selectOne(Integer id);
+	public List<Patient> findAll() ;
+	
+	//根据姓名查找患者的方法
+	public Patient selectOneByName(String name);
+	public String chcekpassword(String name,String password);
+	
+	//找回密码的认证主方法(findPwk)
+	public Patient findPwdCheck(String name,String email);
+	//根据id和键入的password 修改对应id患者的密码(findPwk)
+	public int updatePatientPassword(String id,String password);
 }
